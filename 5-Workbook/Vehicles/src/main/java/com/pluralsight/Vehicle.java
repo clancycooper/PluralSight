80ad20c96a425cdfb06f9e2a72906f5120aadd76
clancycooper <dudeclancy@gmail.com>
2023-11-07 10:01:44 -0500

    Changed to protected properties, removed unnecessary setters

--- 5-Workbook/Vehicles/src/main/java/com/pluralsight/Vehicle.java
+++ 5-Workbook/Vehicles/src/main/java/com/pluralsight/Vehicle.java
@@ -1,8 +1,8 @@
 package com.pluralsight;
 
 public class Vehicle {
-    private String engineType, color;
-    private int speed, carrierCapacity, fuelCapacity, cargoCapacity;
+    protected String engineType, color;
+    protected int speed, carrierCapacity, fuelCapacity, cargoCapacity;
 
     public Vehicle(String engineType, String color, int speed, int carrierCapacity, int fuelCapacity, int cargoCapacity) {
         this.engineType = engineType;
@@ -18,59 +18,32 @@ public class Vehicle {
         return engineType;
     }
 
-    public void setEngineType(String engineType) {
-        this.engineType = engineType;
-    }
-
     public String getColor() {
         return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     public int getSpeed() {
         return speed;
     }
 
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
     public int getCarrierCapacity() {
         return carrierCapacity;
-    }
-
-    public void setCarrierCapacity(int carrierCapacity) {
-        this.carrierCapacity = carrierCapacity;
     }
 
     public int getFuelCapacity() {
         return fuelCapacity;
     }
 
-    public void setFuelCapacity(int fuelCapacity) {
-        this.fuelCapacity = fuelCapacity;
-    }
-
     public int getCargoCapacity() {
         return cargoCapacity;
     }
 
-    public void setCargoCapacity(int cargoCapacity) {
-        this.cargoCapacity = cargoCapacity;
-    }
-
     public void refuel() {
-
     }
 
     public void accelerate() {
-
     }
 
     public void brake() {
-
     }
 }
