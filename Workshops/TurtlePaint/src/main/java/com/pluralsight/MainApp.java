53344e588b3db73eaa9da7859f1a89a7dc19c1fd
clancycooper <dudeclancy@gmail.com>
2023-11-20 15:06:23 -0500

    Finished Square!

--- Workshops/TurtlePaint/src/main/java/com/pluralsight/MainApp.java
+++ Workshops/TurtlePaint/src/main/java/com/pluralsight/MainApp.java
@@ -14,11 +14,12 @@ public class MainApp {
     }
 
     public static void homeScreen() {
-        System.out.println("Welcome to Clancy's Paint-By-Turtle App!\n" +
-                "What would you like to do first?\n" +
-                "A) Add Shape\n" +
-                "S) Save Shape\n" +
-                "X) Exit Program");
+        System.out.println("""
+                Welcome to Clancy's Paint-By-Turtle App!
+                What would you like to do first?
+                A) Add Shape
+                S) Save Shape
+                X) Exit Program""");
         String menuSwap = scanner.nextLine().toUpperCase();
 
         switch (menuSwap) {
@@ -31,14 +32,14 @@ public class MainApp {
         }
     }
 
-
         public static void addShape() {
-            System.out.println("Which shape would you like to paint?\n" +
-                    "C) Circle\n" +
-                    "S) Square\n" +
-                    "R) Rectangle\n" +
-                    "X) Return to Main Menu");
-            String userChoice = scanner.nextLine();
+            System.out.println("""
+                    Which shape would you like to paint?
+                    C) Circle
+                    S) Square
+                    R) Rectangle
+                    X) Return to Main Menu""");
+            String userChoice = scanner.nextLine().toUpperCase();
 
             switch (userChoice) {
                 case "C":
@@ -49,9 +50,9 @@ public class MainApp {
                     scanner.nextLine();
                     System.out.println("What color would you like the border of your circle to be?");
                     String tempColor = scanner.nextLine();
-                    System.out.println("Enter x-coordinate:");
+                    System.out.println("Where would you like your circle to be?\n" + " Enter x-coordinate:");
                     double xCoordinate = scanner.nextDouble();
-                    System.out.println("Enter y-coordinate:");
+                    System.out.println(" Enter y-coordinate:");
                     double yCoordinate = scanner.nextDouble();
                     scanner.nextLine();
                     Point2D.Double location = new Point2D.Double(xCoordinate, yCoordinate);
@@ -62,6 +63,28 @@ public class MainApp {
                     Circle circle = new Circle(turtle, location, border, color, radius);
                     System.out.println("Here comes your turtle painter!");
                     circle.paint();
+                    homeScreen();
+                    break;
+
+                case "S" :
+                    System.out.println("How thick would you like the border width to be?" + "\n (ex: 2)");
+                    int sqBorder = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("What color would you like the border of your square to be?");
+                    String tempSqColor = scanner.nextLine();
+                    System.out.println("Where would you like your square to be?\n" + " Enter the x-coordinate:");
+                    double xSqCoordinate = scanner.nextDouble();
+                    System.out.println(" Enter the y-coordinate:");
+                    double ySqCoordinate = scanner.nextDouble();
+                    scanner.nextLine();
+                    Point2D.Double sqLocation = new Point2D.Double(xSqCoordinate, ySqCoordinate);
+                    Color sqColor = parseColor(tempSqColor);
+
+                    World sqWorld = new World();
+                    Turtle sqTurtle = new Turtle(sqWorld);
+                    Square square = new Square(sqTurtle, sqLocation, sqBorder, sqColor);
+                    System.out.println("Here comes your turtle painter!");
+                    square.paint();
                     homeScreen();
                     break;
             }
--- Workshops/TurtlePaint/src/main/java/com/pluralsight/Square.java
+++ Workshops/TurtlePaint/src/main/java/com/pluralsight/Square.java
@@ -4,11 +4,22 @@ import java.awt.*;
 import java.awt.geom.Point2D;
 
 public class Square extends Shape {
+
     public Square(Turtle turtle, Point2D.Double location, double border, Color color) {
         super(turtle, location, border, color);
     }
 
     public void paint(){
+        turtle.setColor(this.color);
+        turtle.setPenWidth(this.border);
+        turtle.penUp();
+        turtle.goTo(this.location);
+        turtle.penDown();
+        turtle.setDelay(1);
 
+        for (int i = 0; i < 4; i++) {
+            turtle.forward(200);
+            turtle.turnRight(90);
+        }
     }
 }
