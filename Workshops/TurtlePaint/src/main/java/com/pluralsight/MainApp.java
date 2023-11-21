ddef7f30489a4f067527afe0f3c8871a006250d7
clancycooper <dudeclancy@gmail.com>
2023-11-21 11:40:28 -0500

    Added user prompt for canvas size to main method, Added class variables for readability

--- Workshops/TurtlePaint/src/main/java/com/pluralsight/MainApp.java
+++ Workshops/TurtlePaint/src/main/java/com/pluralsight/MainApp.java
@@ -9,8 +9,26 @@ import static com.pluralsight.Shape.parseColor;
 
 public class MainApp {
     public static Scanner scanner = new Scanner(System.in);
+    public static World world;
+    public static Turtle turtle;
+    public static Point2D.Double location;
+    public static int border;
+    public static Color color;
+    public static double xCoordinate, yCoordinate, radius, length, width;
+    public static String tempColor;
+
     public static void main(String[] args) {
-      homeScreen();
+        System.out.println("Please enter the size of your canvas\n" +
+                "Width: ");
+        int width = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Height: ");
+        int height = scanner.nextInt();
+        scanner.nextLine();
+        world = new World(width, height);
+        turtle = new Turtle(world);
+
+        homeScreen();
     }
 
     public static void homeScreen() {
@@ -39,92 +57,89 @@ public class MainApp {
         }
     }
 
-        public static void addShape() {
-            System.out.println("""
+    public static void addShape() {
+        System.out.println("""
                     Which shape would you like to paint?
                     C) Circle
                     S) Square
                     R) Rectangle
                     X) Return to Main Menu""");
-            String userChoice = scanner.nextLine().toUpperCase();
+        String userChoice = scanner.nextLine().toUpperCase();
 
-            switch (userChoice) {
-                case "C":
-                    System.out.println("What is the radius of the circle you'd like the turtle to draw?");
-                    double radius = scanner.nextDouble();
-                    System.out.println("How thick would you like the border width to be?" + "\n (ex: 2)");
-                    int border = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println("What color would you like the border of your circle to be?");
-                    String tempColor = scanner.nextLine();
-                    System.out.println("Where would you like your circle to be?\n" + " Enter x-coordinate:");
-                    double xCoordinate = scanner.nextDouble();
-                    System.out.println(" Enter y-coordinate:");
-                    double yCoordinate = scanner.nextDouble();
-                    scanner.nextLine();
-                    Point2D.Double location = new Point2D.Double(xCoordinate, yCoordinate);
-                    Color color = parseColor(tempColor.toLowerCase());
+        switch (userChoice) {
+            case "C":
+                System.out.println("What is the radius of the circle you'd like the turtle to draw?");
+                radius = scanner.nextDouble();
+                System.out.println("How thick would you like the border width to be?" + "\n (ex: 2)");
+                border = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("What color would you like the border of your circle to be?\n" + "(Choose between: red, blue, green, magenta and cyan)");
+                tempColor = scanner.nextLine();
+                System.out.println("Where would you like your circle to be?\n" + " Enter x-coordinate:");
+                xCoordinate = scanner.nextDouble();
+                System.out.println(" Enter y-coordinate:");
+                yCoordinate = scanner.nextDouble();
+                scanner.nextLine();
+                location = new Point2D.Double(xCoordinate, yCoordinate);
+                color = parseColor(tempColor.toLowerCase());
 
-                    World world = new World();
-                    Turtle turtle = new Turtle(world);
-                    Circle circle = new Circle(turtle, location, border, color, radius);
-                    System.out.println("Here comes your turtle painter!");
-                    circle.paint();
-                    homeScreen();
-                    break;
 
-                case "S" :
-                    System.out.println("How thick would you like the border width to be?" + "\n (ex: 2)");
-                    int sqBorder = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println("What color would you like the border of your square to be?");
-                    String tempSqColor = scanner.nextLine();
-                    System.out.println("Where would you like your square to be?\n" + " Enter the x-coordinate:");
-                    double xSqCoordinate = scanner.nextDouble();
-                    System.out.println(" Enter the y-coordinate:");
-                    double ySqCoordinate = scanner.nextDouble();
-                    scanner.nextLine();
-                    Point2D.Double sqLocation = new Point2D.Double(xSqCoordinate, ySqCoordinate);
-                    Color sqColor = parseColor(tempSqColor);
+                Circle circle = new Circle(turtle, location, border, color, radius);
+                System.out.println("Here comes your turtle painter!");
+                circle.paint();
+                homeScreen();
+                break;
 
-                    World sqWorld = new World();
-                    Turtle sqTurtle = new Turtle(sqWorld);
-                    Square square = new Square(sqTurtle, sqLocation, sqBorder, sqColor);
-                    System.out.println("Here comes your turtle painter!");
-                    square.paint();
-                    homeScreen();
-                    break;
+            case "S" :
+                System.out.println("How thick would you like the border width to be?" + "\n (ex: 2)");
+                border = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("What color would you like the border of your square to be?");
+                tempColor = scanner.nextLine();
+                System.out.println("Where would you like your square to be?\n" + " Enter the x-coordinate:");
+                xCoordinate = scanner.nextDouble();
+                System.out.println(" Enter the y-coordinate:");
+                yCoordinate = scanner.nextDouble();
+                scanner.nextLine();
+                location = new Point2D.Double(xCoordinate, yCoordinate);
+                color = parseColor(tempColor);
 
-                case "R" :
-                    System.out.println("How thick would you like the border width to be?" + "\n (ex: 2)");
-                    int recBorder = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.println("What color would you like the border of your rectangle to be?");
-                    String tempRecColor = scanner.nextLine();
-                    System.out.println("Where would you like your rectangle to be?\n" + " Enter the x-coordinate:");
-                    double xRecCoordinate = scanner.nextDouble();
-                    System.out.println(" Enter the y-coordinate:");
-                    double yRecCoordinate = scanner.nextDouble();
-                    scanner.nextLine();
-                    System.out.println("How long should your rectangle be?");
-                    double length = scanner.nextDouble();
-                    System.out.println("How wide should your rectangle be?");
-                    double width = scanner.nextDouble();
-                    scanner.nextLine();
-                    Point2D.Double recLocation = new Point2D.Double(xRecCoordinate, yRecCoordinate);
-                    Color recColor = parseColor(tempRecColor);
 
-                    World recWorld = new World();
-                    Turtle recTurtle = new Turtle(recWorld);
-                    Rectangle rectangle = new Rectangle(recTurtle, recLocation, recBorder, recColor, length, width);
-                    System.out.println("Here comes your turtle painter!");
-                    rectangle.paint();
-                    homeScreen();
+                Square square = new Square(turtle, location, border, color);
+                System.out.println("Here comes your turtle painter!");
+                square.paint();
+                homeScreen();
+                break;
 
-            }
-        }
+            case "R" :
+                System.out.println("How thick would you like the border width to be?" + "\n (ex: 2)");
+                border = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("What color would you like the border of your rectangle to be?");
+                tempColor = scanner.nextLine();
+                System.out.println("Where would you like your rectangle to be?\n" + " Enter the x-coordinate:");
+                xCoordinate = scanner.nextDouble();
+                System.out.println(" Enter the y-coordinate:");
+                yCoordinate = scanner.nextDouble();
+                scanner.nextLine();
+                System.out.println("How long should your rectangle be?");
+                length = scanner.nextDouble();
+                System.out.println("How wide should your rectangle be?");
+                width = scanner.nextDouble();
+                scanner.nextLine();
+                location = new Point2D.Double(xCoordinate, yCoordinate);
+                color = parseColor(tempColor);
 
-        public static void saveShape() {
+
+                Rectangle rectangle = new Rectangle(turtle, location, border, color, length, width);
+                System.out.println("Here comes your turtle painter!");
+                rectangle.paint();
+                homeScreen();
 
         }
+    }
+
+    public static void saveShape() {
+
+    }
 }
