5316779397fe1715eadf6277aa00132b1b436842
clancycooper <dudeclancy@gmail.com>
2023-11-22 15:03:54 -0500

    added custom user greeting

--- 6-Workbook/GitExercise/src/main/java/org/example/Main.java
+++ 6-Workbook/GitExercise/src/main/java/org/example/Main.java
@@ -3,7 +3,14 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
+    public static String name;
+
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+
         System.out.println("Please enter your name:");
+        name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
