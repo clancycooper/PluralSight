4f100dbfb0f1215c4b7c0aa8afded89ea5ef603d
clancycooper <dudeclancy@gmail.com>
2023-11-22 14:44:42 -0500

    First Commit for Git Exercise

--- 6-Workbook/GitExercise/src/main/java/org/example/Main.java
+++ 6-Workbook/GitExercise/src/main/java/org/example/Main.java
@@ -3,12 +3,7 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-
-    public static Scanner scanner = new Scanner(System.in);
-    public static String name;
     public static void main(String[] args) {
         System.out.println("Please enter your name:");
-        name = scanner.nextLine();
-        System.out.println("Hello " + name + "!");
     }
 }
