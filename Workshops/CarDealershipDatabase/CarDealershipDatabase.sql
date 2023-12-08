6fdbfcfe887a679bff31d5934142ae60d57f6fa9
clancycooper <dudeclancy@gmail.com>
2023-12-08 15:13:46 -0500

    New Main DB file, Finished Workshop

---
+++ Workshops/CarDealershipDatabase/CarDealershipDatabase.sql
@@ -0,0 +1,16 @@
+-- This is my main SQL Script for the Car Dealership DB Workshop
+-- Check the CreateDB Script to see my creation process
+-- Check the Populating Script to see the population process
+-- Check any Query to see my testing process
+
+USE cardealership;
+
+SELECT * FROM dealerships;
+
+SELECT * FROM inventory;
+
+SELECT * FROM lease_contracts;
+
+SELECT * FROM sales_contracts;
+
+SELECT * FROM vehicles;
