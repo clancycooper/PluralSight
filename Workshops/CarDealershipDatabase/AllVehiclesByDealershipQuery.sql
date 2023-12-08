5f080487cc6908749ed58780419759659c2d24d2
clancycooper <dudeclancy@gmail.com>
2023-12-08 14:02:51 -0500

    Finished Find all vehicles for a specific dealership query

---
+++ Workshops/CarDealershipDatabase/AllVehiclesByDealershipQuery.sql
@@ -0,0 +1,21 @@
+USE cardealership;
+
+-- Vehicles for the Dealership 'Clancys Coopers'
+SELECT
+	v.*, d.name
+FROM
+	vehicles v
+INNER JOIN dealerships d
+	ON v.dealership_id = d.dealership_id
+WHERE
+	d.name = 'Clancys Coopers';
+
+-- Vehicles for the Dealership 'Scout Used Cars
+SELECT
+	v.*, d.name
+FROM
+	vehicles v
+INNER JOIN dealerships d
+	ON v.dealership_id = d.dealership_id
+WHERE
+	d.name = 'Scout Used Cars';
