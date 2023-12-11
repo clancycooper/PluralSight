32d5063bd7c723188a5bf39e2b18011d5be33403
clancycooper <dudeclancy@gmail.com>
2023-12-11 15:43:22 -0500

    Finished exercise, hid pw, made username EV

--- 8-Workbook/NorthWindProducts/src/main/java/com/pluralsight/Main.java
+++ 8-Workbook/NorthWindProducts/src/main/java/com/pluralsight/Main.java
@@ -6,27 +6,31 @@ public class Main {
     public static void main(String[] args) {
 
         String url = "jdbc:mysql://127.0.0.1:3306/northwind";
-        String user = "root";
-        String password = "-redacted-";
+        String username = System.getenv("mysql_user");
+        String password = System.getenv("-redacted-");
 
         String query = "SELECT * FROM Products";
         try {
             // Establishing connection
-            Connection conn = DriverManager.getConnection(url, user, password);
-            Statement stmt = conn.createStatement();
+            Connection conn = DriverManager.getConnection(url, username, password);
+            PreparedStatement prepStatement = conn.prepareStatement("SELECT * FROM products;");
 
             // Executing query
-            ResultSet rs = stmt.executeQuery(query);
+            ResultSet result = prepStatement.executeQuery(query);
 
             // Processing the result set
-            while (rs.next()) {
+            while (result.next()) {
                 // Replace with your column names and types
-                System.out.println(rs.getString(2));
+                System.out.println("ID:       " + result.getInt(1));
+                System.out.println("Name:     " + result.getString(2));
+                System.out.println("Price:    " + result.getDouble(6));
+                System.out.println("Stock:    " + result.getInt(7));
+                System.out.println(" -------- ");
             }
 
             // Closing resources
-            rs.close();
-            stmt.close();
+            result.close();
+            prepStatement.close();
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
