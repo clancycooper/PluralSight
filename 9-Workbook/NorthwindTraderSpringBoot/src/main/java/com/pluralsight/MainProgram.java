d157961f870092251115852d2396036dfe6c3055
clancycooper <dudeclancy@gmail.com>
2023-12-19 12:11:47 -0500

    Finished Exercise to make app run from Main to NorthwindApp

---
+++ 9-Workbook/NorthwindTraderSpringBoot/src/main/java/com/pluralsight/MainProgram.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import com.pluralsight.service.ProductService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MainProgram {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MainProgram.class, args);
+	}
+
+}
---
+++ 9-Workbook/NorthwindTraderSpringBoot/src/main/java/com/pluralsight/NorthwindApplication.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import com.pluralsight.dao.ProductDAOInt;
+import com.pluralsight.model.Product;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class NorthwindApplication implements CommandLineRunner {
+
+    private final ProductDAOInt productDAOInt;
+
+    public NorthwindApplication(ProductDAOInt productDAOInt) {
+        this.productDAOInt = productDAOInt;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Product> products = productDAOInt.getAll();
+        products.forEach(product -> System.out.println(product.toString()));
+    }
+}
+
